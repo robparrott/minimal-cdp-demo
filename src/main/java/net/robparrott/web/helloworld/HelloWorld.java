edd2c37ab2e3932c3369bcdbfd258efe7a08f959
Rob Parrott <robparrott@gmail.com>
2014-10-14 17:19:04 -0400

    Change msg

--- src/main/java/net/robparrott/web/helloworld/HelloWorld.java
+++ src/main/java/net/robparrott/web/helloworld/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getWriter().print("Hello from an Java HttpServlet!\nAutodeployed to Heroku!\n");
+        resp.getWriter().print("Hello from an Java HttpServlet!\nAutodeployed to Heroku!\nAdded to S3 Bucket\n");
     }
 
     public static void main(String[] args) throws Exception {
