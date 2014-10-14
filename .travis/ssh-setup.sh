b10d254e15365eb69ca8662cbee9f9362781ef93
Rob Parrott <robparrott@gmail.com>
2014-10-14 13:29:09 -0400

    add ssh setup wrapper script

---
+++ .travis/ssh-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Sets up SSH within the Travis CI service 
+#   so that we can use the private SSH key push to repos
+#
+# Assume we run from the root of the repo under Travis CI
+#
+
+# Run ssh-agent
+eval `ssh-agent`
+
+# Add the key
+chmod 600 .travis/web-helloworld-production-deploy
+ssh-add .travis/web-helloworld-production-deploy
+ssh-add -l 
+
+
+
